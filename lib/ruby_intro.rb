4eed2bc32041ff241bbd37eb76db5b51bdf2822f
Ubuntu <ubuntu@ip-172-31-29-22.us-east-2.compute.internal>
2019-09-17 04:17:52 +0000

    Finished

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -10,29 +10,37 @@ def sum arr
 end
 
 def max_2_sum arr
-  large1 = -1/0
-  large2 = -1/0
+  large1 = -1.0/0
+  large2 = -1.0/0
   for i in arr
-    if arr[i] >= large1
+    if i >= large1
       large2 = large1
-      large1 = arr[i]
-    elsif arr[i] > large2
-      large2 = arr[i]
+      large1 = i
+    elsif i > large2
+      large2 = i
     end
   end
   
-  return large1 + large2
+  if large2 > (-1.0/0)
+    return large1 + large2
+  elsif large1 > (-1.0/0)
+    return large1
+  else
+    return 0
+  end
 end
 
 def sum_to_n? arr, n
   for i in arr
     for j in arr
-      sum = arr[i] + arr[j]
+      sum = i + j
       if i != j and sum == n
         return true
       end
     end
   end
+  
+  return false
 end
 
 # Part 2
@@ -43,9 +51,16 @@ def hello(name)
   return fullGreeting
 end
 
+
+
 def starts_with_consonant? s
+  # Handle empty case
+  if s[0] == nil
+    return false
+  end
+  
   lower = s.downcase
-  letters = lower.split
+  letters = lower.split("")
   letter = letters[0]
   
   if letter < 'a' or letter > 'z'
@@ -59,12 +74,70 @@ def starts_with_consonant? s
   return true
 end
 
+
+
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  charArray = s.split("")
+  print charArray
+  
+  charArray.each do |digit|
+    if (digit != "0") and (digit != "1")
+      return false
+    end
+  end
+  
+# Handle 0 case
+  if charArray.length == 1 and charArray[0] == '0'
+    return true
+  end
+  
+  last2 = charArray.last(2)
+  
+  if (last2[0] == '0') and (last2[1] == '0')
+    return true
+  else
+    return false
+  end
 end
+
+
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  def initialize (isbn_string, prc)
+    @isbn = isbn_string
+    @price = prc
+    
+    if @isbn[0] == nil
+      raise ArgumentError, 'ISBN is empty string'
+    end
+    
+    if @price <= 0
+      raise ArgumentError, 'Price <= 0'
+    end
+  end
+  
+  # Getters
+  def isbn
+    @isbn
+  end
+  
+  def price
+    @price
+  end
+  
+  # Setters
+  def isbn=(isbn)
+    @isbn = isbn
+  end
+  
+  def price=(price)
+    @price = price
+  end
+  
+  # Price as string
+  def price_as_string
+    return "$%.2f" % [@price]
+  end
 end
